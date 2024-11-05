df575f45cf91ee51fe0686674e796019e6527435
Julia Martins <juliamartins.td2.senai@gmail.com>
2024-11-05 14:58:04 -0300

    feat insert

--- julia_martins_sql.sql
+++ julia_martins_sql.sql
@@ -16,3 +16,19 @@ CREATE TABLE medicos(
 
 SELECT * FROM medicos;
 
+INSERT INTO pacientes (nome, email) VALUES ('Thiago', 'thiago.rodrigues@gmail.com');
+INSERT INTO pacientes (nome, email) VALUES ('Felipe', 'felipe.dev@gmail.com');
+INSERT INTO pacientes (nome, email) VALUES ('Julia', 'julia.martins@gmail.com');
+INSERT INTO pacientes (nome, email) VALUES ('João', 'joao.silva@gmail.com');
+INSERT INTO pacientes (nome, email) VALUES ('Pedro', 'pedro.silva@gmail.com');
+
+SELECT * FROM pacientes;
+
+INSERT INTO medicos (nome, especialidade) VALUES 
+('Dr. Paulo', 'Cardiologista'),
+('Dra. Rita', 'Dermatologista'),
+('Dra. Carmen', 'Pediatria'),
+('Dr. Ricardo', 'Homeopata'),
+('Dr. Vitor', 'Neurologista'),
+
+SELECT * FROM medicos;
